81c1bfdc6e555e8157ff548acc01a292b8f1fe82
osama78assi <osama78assi@gmail.com>
2024-12-14 00:14:41 +0400

    fixing

--- assets/Flag-Dv6UbKx1.js
+++
@@ -1,9 +0,0 @@
-import{a,b as d}from"./index-YsPsgoZE.js";const o=a.img`
-  max-width: 2rem;
-  border-radius: var(--border-radius-tiny);
-  display: block;
-  border: 1px solid var(--color-grey-100);
-  ${r=>r.$additionStyle&&d`
-      ${r.$additionStyle}
-    `}
-`;export{o as F};
---
+++ assets/Pagination-DTbYOGdn.js
@@ -0,0 +1,49 @@
+import{a as o,b as h,m as p,j as e,H as m,e as x}from"./index-DKoFbsHf.js";const f=o.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`,u=o.p`
+  font-size: 1.4rem;
+  margin-left: 0.8rem;
+
+  & span {
+    font-weight: 600;
+  }
+  ${n=>n.$singlePage&&h`
+      width: 100%;
+      text-align: center;
+    `}
+`,l=o.button`
+  background-color: ${n=>n.active?" var(--color-brand-600)":"var(--color-grey-50)"};
+  color: ${n=>n.active?" var(--color-brand-50)":"inherit"};
+  border: none;
+  border-radius: var(--border-radius-sm);
+  font-weight: 500;
+  font-size: 1.4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.4rem;
+  padding: 0.6rem 1.2rem;
+  transition: all 0.3s;
+
+  &:has(span:last-child) {
+    padding-left: 0.4rem;
+  }
+
+  &:has(span:first-child) {
+    padding-right: 0.4rem;
+  }
+
+  & svg {
+    height: 1.8rem;
+    width: 1.8rem;
+  }
+
+  &:hover:not(:disabled) {
+    background-color: var(--color-brand-600);
+    color: var(--color-brand-50);
+  }
+`;function b({count:n,pageSize:s}){const[t,c]=p(),r=t.get("page")?+t.get("page"):1,a=Math.ceil(n/s);function d(){if(r===a)return;const i=r+1;t.set("page",i),c(t)}function g(){if(r==1)return;const i=r-1;t.set("page",i),c(t)}return e.jsxs(f,{children:[a>1&&e.jsxs(l,{onClick:g,disabled:r===1,children:[e.jsx(m,{}),e.jsx("span",{children:"Previous"})]}),e.jsxs(u,{$singlePage:a===1,children:["Showing ",e.jsx("span",{children:(r-1)*s+1})," to"," ",e.jsx("span",{children:r===a?n:r*s})," ","of ",e.jsxs("span",{children:[n," "]})," results"]}),a>1&&e.jsxs(l,{onClick:d,disabled:r===a,children:[e.jsx(x,{}),e.jsx("span",{children:"Next"})]})]})}export{b as P};
